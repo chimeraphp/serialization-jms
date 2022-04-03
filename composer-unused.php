8ec1d4b4a6c958805b6708ed82a481266a484074
Luís Cobucci <lcobucci@gmail.com>
2022-04-03 20:14:55 +0200

    Fix composer-unused configuration
    It seems like a new version has been released and it partially solved
    the issue.

--- composer-unused.php
+++ composer-unused.php
@@ -8,6 +8,5 @@ use ComposerUnused\ComposerUnused\Configuration\NamedFilter;
 // @see https://github.com/composer-unused/composer-unused/issues/326
 
 return static fn(Configuration $config): Configuration => $config
-    ->addNamedFilter(NamedFilter::fromString('php'))
     ->addNamedFilter(NamedFilter::fromString('chimera/foundation'))
     ->addNamedFilter(NamedFilter::fromString('jms/serializer'));
